216c30dcd7d66836b934fa8b89fb064188adae86
Lennart Jarsetz <lennart.jarsetz@sva.de>
2026-02-23 10:28:56 +0100

    Added set_hostname + dev_null options

---
+++ linux/commands/dev_null_variants.sh
@@ -0,0 +1,5 @@
+# Ways to link output to /dev/null
+
+> /dev/null 2>&1
+&> /dev/null
+1>/dev/null 2>/dev/null
---
+++ linux/commands/set_hostname.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo hostnamectl set-hostname <your_hostname>
+
+# check with
+hostnamectl
