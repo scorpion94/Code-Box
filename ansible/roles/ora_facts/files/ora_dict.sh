2ad05e9b0fe078e174bb7a1be59ed25b3c76de37
Lennart Jarsetz <lennart.jarsetz@sva.de>
2026-02-23 10:34:48 +0100

    Changed ora_dict.sh so it aborts + added tasks to create ora_facts_dict

--- ansible/roles/ora_facts/files/ora_dict.sh
+++ ansible/roles/ora_facts/files/ora_dict.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
-db_array=()
-db_list=($(cat ${oratab} | grep -v '^\s*#' | sed '/^$/d' | grep -iv DUMMY | awk -F: '{print $1}'))
+
+if [[ -z "${oratab}" || ! -e "${oratab}" ]]
+then
+  echo "Variable oratab is not definied or does not exists"
+  exit 1
+fi
+
+# shellcheck disable=SC2207
+# Currently not sure if mapfile would be a good idea since it might need to run on Solaris as well
+db_list=($(cat "${oratab}" | grep -v '^\s*#' | sed '/^$/d' | grep -iv DUMMY | awk -F: '{print $1}'))
+
+check_db_is_running () {
+  local db_name=$1
+
+  if pgrep -f "ora_pmon_${db_name}" > /dev/null 2>&1
+  then
+    db_running_state=true
+  fi
+}
 
 get_pdb_name () {
  local db_name=$1
 
- . oraenv <<< "${db_name}" >/dev/null 2>&1
+ source /usr/local/bin/oraenv <<< "${db_name}" >/dev/null 2>&1
  pdb_list=$(sqlplus -s / as sysdba <<EOF
+   WHENEVER SQLERROR EXIT 1;
    set echo off;
    set heading off;
    set feedback off;
@@ -20,7 +38,7 @@ EOF
 get_version () {
  local db_name=$1
 
- . oraenv <<< "${db_name}" >/dev/null 2>&1
+ source /usr/local/bin/oraenv <<< "${db_name}" >/dev/null 2>&1
  db_version=$(sqlplus -s / as sysdba <<EOF
    set echo off;
    set heading off;
@@ -33,19 +51,43 @@ EOF
 
 get_oracle_home () {
  local db_name=$1
- db_home=$(grep ${db_name} ${oratab}| awk -F: '{print $2}') 
+ db_home=$(grep -w "${db_name}" "${oratab}" | awk -F: '{print $2}') 
 }
 
 
+# Example output
+# echo "DB:DB_HOME:DB-VERISON:PDB:PDB:PDB"
+# {"CORCL":{"home":"/u01/product/db_lnx_1919","version":"19.19.0.0.0","pdbs":["ORCL","PDB$SEED"]}}
 
-# echo "DB:DB-VERISON:DB_HOME:PDB:PDB:PDB"
 for db in "${db_list[@]}"
 do
- get_pdb_name ${db}
- get_version ${db}
- get_oracle_home ${db}
- echo "${db}:${db_home}:${db_version}:${pdb_list}"
- pdb_list=""
- db_version=""
- db_home=""
+  get_oracle_home "${db}"
+  check_db_is_running "${db}"
+  if [[ "${db_running_state}" == "true" ]]
+  then
+    get_pdb_name "${db}"
+    get_version "${db}"
+  fi
+  
+  # Note jq -cM 
+  # -c -> compact output instead of pretty
+  # -M monochrome -> To avoid ansicode like \u001b[0m\
+  # Otherwise its not parsable later in ansible because the result looks like this:
+  # "stdout": "\u001b[1;39m{\r\n \u001b[0m\u001b[34;1m\"db_name\"\u001b[0m\u001b[1;39m: 
+  #   \u001b[0m\u001b[0;32m\"CORCL\"\u001b[0m\u001b[1;39m,\r\n \u001b[0m\u001b[34;1m\"db_home\"\u001b[0m\u001b[1;39m:
+  echo "${db}:${db_home}:${db_version}:${pdb_list}" \
+    | jq -R -cM 'split(":") | 
+      {
+        (.[0]): {
+            home: .[1],
+            version: (if .[2] == "" then null else .[2] end), # Get second item, if item is empty return null
+            pdbs: (.[3:]| map(if . == "" then empty else . end)) # Get 3rd ++ item, if none then return empty list
+        }
+      }'
+
+  # null variables for next loop
+  db_running_state=""
+  pdb_list=""
+  db_version=""
+  db_home=""
 done
