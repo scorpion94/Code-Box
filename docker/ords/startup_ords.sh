86fa789d4302798be267bada4b9df5f9ad9d5959
Lennart <lennart@jarsetz.hamburg>
2026-03-04 07:27:38 +0100

    Added check for port opening

--- docker/ords/startup_ords.sh
+++ docker/ords/startup_ords.sh
@@ -2,6 +2,18 @@
 
 ORDS_CONFIG=${ORDS_CONFIG:-/u01/config_ords}
 ORDS_PATH=${ORDS_PATH:-/u01/ords}
+DB_HOST=${DB_HOST:-192.168.56.10}
+DB_PORT=${DB_PORT:-1521}
+
+if echo > /dev/tcp/"${DB_HOST}"/"${DB_PORT}" 2>/dev/null
+then
+  echo "Port open"
+else
+  echo "Port closed"
+  exit 1
+fi
+
+
 
 echo "=====DISPLAY ORDS CONFIG====="
 "${ORDS_PATH}"/bin/ords --config "${ORDS_CONFIG}" config list
