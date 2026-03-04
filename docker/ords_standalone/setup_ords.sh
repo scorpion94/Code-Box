2502b83546eac5239a92e20f9db1de70db3cde56
Lennart <lennart@jarsetz.hamburg>
2026-03-04 18:03:42 +0100

    Renamed ords to ords_standalone

--- docker/ords_standalone/setup_ords.sh
+++ docker/ords_standalone/setup_ords.sh
@@ -5,6 +5,7 @@ ORDS_PORT=${ORDS_PORT:-8080}
 ORDS_LOGPATH=${ORDS_LOGPATH:-/u01/logs_ords}
 DB_HOST=${DB_HOST:-192.168.56.10}
 DB_PORT=${DB_PORT:-1521}
+DB_SERVICENAME=${DB_SERVICENAME:-ORCL}
 APEX_PATH=${APEX_PATH:-/u01/apex_242/}
 
 if echo > /dev/tcp/"${DB_HOST}"/"${DB_PORT}" 2>/dev/null
@@ -15,11 +16,13 @@ else
   exit 1
 fi
 
+echo "Hostname: ${DB_HOST} Port: ${DB_PORT} Servicename: ${DB_SERVICENAME}"
+
 "${ORDS_PATH}"/bin/ords --config "${ORDS_CONFIG}" install \
   --admin-user SYS \
   --db-hostname "${DB_HOST}" \
   --db-port "${DB_PORT}" \
-  --db-servicename ORCL \
+  --db-servicename "${DB_SERVICENAME}" \
   --proxy-user \
   --db-user ORDS_PUBLIC_USER \
   --feature-db-api true \
